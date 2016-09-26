7a92da82e3d91dffe8ac95543317b01b984cccbf
LegendKrazy <kdaniti@gmail.com>
2016-09-25 21:18:43 -0400

    path_finder / neighbor checker taking "shape"

--- Hypersonic.py
+++ Hypersonic.py
@@ -1,19 +1,18 @@
 import sys
 width, height, my_id = [int(i) for i in input().split()]
 turns = 0  # TODO: Think of a better way to find out starting position on opening turn only
+startPos = 0
 # game loop
 while True:
     turns += 1
     # print("Turn: ",turns, file=sys.stderr)
-    board = [[x for x in input()] for y in range(height)]  # a list of lists to show what is on the board
+    grid = [[x for x in input()] for y in range(height)]  # a list of lists to show what is on the board
     entities = int(input())  # Current Players and Bomb Count on the board.
-    # print("Entities: ", entities, file=sys.stderr)
     for i in range(entities):
-        # print("Ent: ", i, file=sys.stderr)
-        entity_type, owner, x, y, bombCount, blastRange = [j for j in input().split()]
-        if turns == 1 and owner == 0:  # ONLY on Turn 1 assign our starting position
-            startPos = [x, y]
-            # print("startPos: ", startPos, file=sys.stderr)
+        entity_type, owner, x, y, bombCount, blastRange = [int(j) for j in input().split()]
+        if owner == 0 and turns <= 1:  # ONLY on Turn 1 assign our starting position
+            startPos = x,y
+            print("Set Pos: ", startPos, file=sys.stderr)
         # My Debug Console
         """
         print("******************", file=sys.stderr)
@@ -25,11 +24,22 @@ while True:
         print("Turns : ", turns, file=sys.stderr)
         print("******************", file=sys.stderr)
         """
-    def path_finder(): # TODO: Let's determine the closest position to blow up the most boxes
-        for i in board: # TODO: Compare each row to combo boxes within 3 spaces for max points
-            for x in i:
-                print("X: ", x, file=sys.stderr)
-    path_finder()
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr)
+    def path_finder(x, y): # TODO: Let's determine the closest position to blow up the most boxes
+        print("Grid XY: ", type(grid[x][y]), file=sys.stderr)
+        # explore neighbors clockwise starting by the one on the right
+        if ((x < len(grid) - 1 and path_finder(x + 1, y))
+            or (y > 0 and path_finder(x, y - 1))
+            or (x > 0 and path_finder(x - 1, y))
+            or (y < len(grid) - 1 and path_finder(x, y + 1))):
+            print("BOMB %d %d" % (x, y))
+        else:
+            newPos = x, y
+            print("MOVE 9 10")
+
+        return False
+        # for i in board: # TODO: Compare each row to combo boxes within 3 spaces for max points
+        #    for x in i:
+    # path_finder(startPos)
+    # print("Path_Finder: ", path_finder(startPos), file=sys.stderr)
+    print("startPos Out: ", path_finder(*startPos), file=sys.stderr)
     print("MOVE 9 10")
