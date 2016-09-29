0cd6085ad6df7bfbd0128a025fd7d63e1c4c02fd
LegendKrazy <kdaniti@gmail.com>
2016-09-29 12:41:20 -0400

    Detect enemy bomb when dropped

--- Hypersonic.py
+++ Hypersonic.py
@@ -9,18 +9,22 @@ def path_finder(x, y):  #TODO: Let's determine the closest position to blow up t
     #@TODO Pathfinder memes
     return x, y
 
-def start_pos(x,y):  # Identify where we started
+def start_pos(x,y):
+    # Identify where we started
     startPos = x,y
     beginPos = True
     print("Set Pos: ", startPos, file=sys.stderr)
     print("Begin Pos: ", beginPos, file=sys.stderr)
 
 def moved_self(x,y):
+    beginPos = False
     newPos = x,y
     print("moved_self: ", beginPos, file=sys.stderr)
 
 def getSafe(x,y): # Check if character is in range and in danger, if yes MOVE to safety
     print("getSafe:", x, y, file=sys.stderr)
+    print("It worked")
+    '''
     if newPos[0] and newPos[1] == (x,y):
         pass
     if newPos[0] and newPos[1] == (x+1,y+1):
@@ -37,6 +41,7 @@ def getSafe(x,y): # Check if character is in range and in danger, if yes MOVE to
         pass
     else:
         pass
+    '''
 while True:  # game loop for Bomberman clone contest: 400 turns and it ends
     turns += 1
     # print("Turn: ",turns, file=sys.stderr)
@@ -44,30 +49,32 @@ while True:  # game loop for Bomberman clone contest: 400 turns and it ends
     # turn grid into coordinates
     match = [(i, j) for i, row in enumerate(grid) for j, cell in enumerate(row) if cell == '.']
     print(match, file=sys.stderr)
-    entities = int(input())  # Current Players and Bomb Count on the board.
+
+    # Current loop game information
+    entities = int(input())
     for i in range(entities):
         entity_type, owner, x, y, bombCount, blastRange = [int(j) for j in input().split()]
-
         if owner == 1 and bombCount == 0:
+            # Detects if they dropped a bomb on the current tile
             getSafe(x,y)
+            bombLocation = (x,y)
+            print("bombLocation: ", bombLocation, file=sys.stderr)
         if owner == 0 and turns <= 1:  # ONLY on Turn 1 assign our starting position
-            startPos9(x,y)
-            print("Set Pos: ", startPos, file=sys.stderr)
+            start_pos(x, y)
         elif owner == 0 and turns >= 2: # assign position each turn
             moved_self(x,y)
-            print("Current X,Y: ", x, y, file=sys.stderr)
-    if beginPos:
-        pass
-        # My Debug Console
-        """
-        print("******************", file=sys.stderr)
-        print("Entity_Type: ", entity_type, file=sys.stderr)
-        print("Owner: ", owner, file=sys.stderr)
-        print("Bomb Count : ", bombCount, file=sys.stderr)
-        print("Blast Range : ", blastRange, file=sys.stderr)
-        print("Turns : ", turns, file=sys.stderr)
-        print("******************", file=sys.stderr)
-        """
+    print("MOVE 8 10")
+
+    # My Debug Console
+    """
+    print("******************", file=sys.stderr)
+    print("Entity_Type: ", entity_type, file=sys.stderr)
+    print("Owner: ", owner, file=sys.stderr)
+    print("Bomb Count : ", bombCount, file=sys.stderr)
+    print("Blast Range : ", blastRange, file=sys.stderr)
+    print("Turns : ", turns, file=sys.stderr)
+    print("******************", file=sys.stderr)
+    """
 
     # path_finder(startPos)
     # print("Path_Finder: ", path_finder(startPos), file=sys.stderr)
